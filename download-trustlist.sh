04131188cd0361de2e7a24b6124732c900439ce9
Janno Põldma <janno.poldma@gmail.com>
2026-07-31 22:06:51 +0300

    Use bash script

---
+++ download-trustlist.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+pushd $TRUSTLIST_DOWNLOAD_DIR
+
+printf "Downloading https://ec.europa.eu/tools/lotl/eu-lotl.xml ...\n"
+curl -fLO https://ec.europa.eu/tools/lotl/eu-lotl.xml
+
+xmllint --xpath "//*[local-name() = 'TrustServiceStatusList']/*[local-name() = 'SchemeInformation']/*[local-name() = 'SchemeInformationURI']/*[local-name() = 'URI']/text()" eu-lotl.xml \
+    | grep '\.xml$' \
+    |
+while IFS= read -r file; do
+    printf "Downloading $file ...\n"
+    curl -fLO "$file"
+done
+
+mapfile -t urls < <(xmllint --xpath "//*[local-name() = 'TrustServiceStatusList']/*[local-name() = 'SchemeInformation']/*[local-name() = 'PointersToOtherTSL']/*[local-name() = 'OtherTSLPointer']/*[local-name() = 'TSLLocation']/text()" eu-lotl.xml)
+mapfile -t territories < <(xmllint --xpath "//*[local-name() = 'TrustServiceStatusList']/*[local-name() = 'SchemeInformation']/*[local-name() = 'PointersToOtherTSL']/*[local-name() = 'OtherTSLPointer']/*[local-name() = 'AdditionalInformation']/*[local-name() = 'OtherInformation']/*[local-name() = 'SchemeTerritory']/text()" eu-lotl.xml)
+mapfile -t mime_types < <(xmllint --xpath "//*[local-name() = 'TrustServiceStatusList']/*[local-name() = 'SchemeInformation']/*[local-name() = 'PointersToOtherTSL']/*[local-name() = 'OtherTSLPointer']/*[local-name() = 'AdditionalInformation']/*[local-name() = 'OtherInformation']/*[local-name() = 'MimeType']/text()" eu-lotl.xml)
+
+
+for i in "${!urls[@]}"; do
+    if [[ "${mime_types[$i]}" == "application/vnd.etsi.tsl+xml" ]] ; then
+        if [[ "${territories[$i]}" != "EU" ]]; then
+            printf "Downloading ${urls[$i]} => ${territories[$i]}.xml ...\n"
+            curl -fL "${urls[$i]}" -o "${territories[$i]}.xml"
+        fi
+    fi
+done
+
+popd
